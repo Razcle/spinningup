c93d0d47ed0a59193c759c4997bcf968511c7e81
AVMCMC <annonymous@anon.com>
2019-01-22 11:25:37 +0000

    updated dqn code

--- spinup/examples/simple_dqn/raza_dqn.py
+++ spinup/examples/simple_dqn/raza_dqn.py
@@ -2,26 +2,26 @@ import tensorflow as tf
 import numpy as np
 import gym
 import random
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from collections import deque
 
-plt.ion()
-
 
 class Experience:
-    def __init__(self, observation, action, reward, terminal):
+    def __init__(self, observation, action, reward, next_observation):
         self.observation = observation
         self.action = action
         self.reward = reward
-        self.terminal = terminal
+        self.next_observation = next_observation
 
-        @property
-        def is_terminal(self):
-            return self.next_observation is None
+    @property
+    def is_terminal(self):
+        return self.next_observation is None
 
 
 class ReplayBuffer:
-    def __init__(self, size=100):
+    def __init__(self, size=50000):
         self.size = size
         self._experiences = []
         self._next_to_overwrite = 0
@@ -45,8 +45,8 @@ class ReplayBuffer:
 
 class MyEnvironment:
 
-    def __init__(self, env, state_size=4):
-        self.env = env
+    def __init__(self, env_name, state_size=4):
+        self.env = gym.make(env_name)
         self.state_size = state_size
         self.obs_buffer = deque(maxlen=state_size)
 
@@ -64,59 +64,6 @@ class MyEnvironment:
             self.obs_buffer.append(obs)
         return list(self.obs_buffer)
 
-# class ExperienceBuffer:
-#
-#     def __init__(self,  obs_dim, max_size=10000, state_size=1):
-#         self.max_size = max_size
-#         self.observations = np.empty((max_size,) + obs_dim)
-#         self.rewards = np.empty((max_size,))
-#         self.actions = np.empty((max_size,))
-#         self.terminals = np.empty((max_size,), dtype=bool)
-#         self.state_size = state_size
-#         self.current = 0
-#         self.count = 0
-#
-#
-#     def add_to_buffer(self, obs, action, reward, is_terminal):
-#             self.observations[self.current] = obs
-#             self.rewards[self.current] = reward
-#             self.actions[self.current] = action
-#             self.terminals[self.current] = is_terminal
-#             self.current = (self.current + 1) % self.max_size
-#             self.count +=1
-#
-#     def _get_index(self):
-#         suitable = False
-#         while not suitable:
-#             init_ind = np.random.randint(0, min(self.count, self.max_size) - self.state_size)
-#             if not np.any(self.terminals[init_ind: init_ind + self.state_size]):
-#                 suitable = True
-#         return init_ind
-#
-#     def get_last_state(self):
-#         if self.current > self.state_size:
-#             return np.stack(self.observations[self.current - self.state_size: self.current], axis=-1)[np.newaxis, :]
-#         else:
-#             return np.stack(self.observations[self.max_size - self.state_size: self.max_size], axis=-1)[np.newaxis, :]
-#
-#     def get_batch(self,  batch_size=32):
-#
-#         state_batch = []
-#         next_state_batch = []
-#         rewards_batch = []
-#         terminal_batch = []
-#         action_batch = []
-#
-#         while len(state_batch) < batch_size:
-#             init_ind = self._get_index()
-#             state_batch.append(np.stack(self.observations[init_ind: init_ind + self.state_size], axis=-1))
-#             next_state_batch.append(np.stack(self.observations[init_ind + 1: init_ind + self.state_size + 1], axis=-1))
-#             terminal_batch.append(self.terminals[init_ind + self.state_size])
-#             action_batch.append(self.actions[init_ind + self.state_size -1])
-#             rewards_batch.append(self.rewards[init_ind + self.state_size - 1])
-#
-#         return np.array(state_batch), np.array(next_state_batch), np.array(action_batch), np.array(rewards_batch), np.array(terminal_batch)
-
 
 def q_network(x, num_actions, activation=tf.nn.relu, output_activation=None):
     """ Takes in a state of shape (batch, 105, 80, 4) and returns an estimate
@@ -128,7 +75,7 @@ def q_network(x, num_actions, activation=tf.nn.relu, output_activation=None):
     # x = tf.layers.conv2d(x, filters=64, kernel_size=3,
     #                      strides=1, activation=activation)
     x = tf.layers.flatten(x)
-    x = tf.layers.dense(x, 32, activation=activation)
+    x = tf.layers.dense(x, 50, activation=activation)
     return tf.layers.dense(x, num_actions, activation=output_activation)
 
 
@@ -161,11 +108,12 @@ def preprocess(img):
     #
     return img
 
+
 def train(env_name='CartPole-v0',  batch_size=32, discount=0.99, lr=1e-3, render=True, buffer_size=50000,
           target_update_freq=1000, max_actions=10000000, state_size=4):
 
     # get the environment
-    env = MyEnvironment(gym.make(env_name), state_size=state_size)
+    env = MyEnvironment(env_name, state_size=state_size)
     num_actions = env.env.action_space.n
     obs_dim = env.env.observation_space.shape
     fig, ax = plt.subplots()
@@ -181,7 +129,7 @@ def train(env_name='CartPole-v0',  batch_size=32, discount=0.99, lr=1e-3, render
         actions = tf.argmax(action_values, axis=1)
         taken_action_values = tf.reduce_sum(action_values * tf.one_hot(actions_taken, num_actions), axis=1)
     with tf.variable_scope('target_network', reuse=tf.AUTO_REUSE):
-        target_action_values = tf.reduce_max((q_network(state_ph, num_actions)), axis=1)
+        target_action_values = tf.reduce_max((q_network(state_ph, num_actions)), axis=1, keepdims=False)
 
     loss = tf.reduce_mean((targets_ph - taken_action_values)**2, axis=0)
 
@@ -198,29 +146,52 @@ def train(env_name='CartPole-v0',  batch_size=32, discount=0.99, lr=1e-3, render
     buffer = ReplayBuffer(size=buffer_size)
 
     # initialise episode-specific variables
+    epsilon = 1.0
     observation = preprocess(env.reset())  # first obs comes from starting distribution
     done = False  # signal from environment that episode is over
 
     # render first episode of each epoch
     finished_rendering_this_epoch = False
 
-    def act_epsilon_greedy(t, exp_buffer, init_epsilon=1.0, final_epsilon=0.001, decay_steps=10000):
-        epsilon = ((final_epsilon - init_epsilon)/decay_steps) * t + init_epsilon
+    def act_epsilon_greedy(epsilon, observation):
         if np.random.rand() < epsilon:
             return np.random.randint(0, num_actions)
         else:
-            state = exp_buffer.get_last_state()
+            state = np.stack(observation, axis=-1)[np.newaxis, :]
             return sess.run(actions, feed_dict={state_ph: state})[0]
+
+    def update_epsilon(t, epsilon, init_epsilon=1.0, final_epsilon=0.01, decay_steps=50000):
+        if epsilon > 0.1:
+            epsilon = ((final_epsilon - init_epsilon)/decay_steps) * t + init_epsilon
+        return epsilon
+
+
+    def get_targets(experiences):
+        state_batch = []
+        target_batch = []
+        actions_batch = []
+        for experience in experiences:
+            state_batch.append(experience.observation)
+            actions_batch.append(action)
+            if experience.is_terminal:
+                target_batch.append(experience.reward)
+            else:
+                next_av = sess.run(target_action_values,
+                                   feed_dict= {state_ph:np.array(experience.next_observation)[np.newaxis, :]})
+                target_batch.append(experience.reward + discount * next_av[0])
+        return np.array(state_batch), np.array(actions_batch), np.array(target_batch)
+
 
     ret = 0
     while total_actions < max_actions:
 
         # rendering
         if (not finished_rendering_this_epoch) and render:
-            env.render()
+            env.env.render()
 
-        action = act_epsilon_greedy(total_actions, buffer)
-        next_observation, reward, done, info = (None, 0., True, None) if done else env.step(action)
+        action = act_epsilon_greedy(epsilon, observation)
+        epsilon = update_epsilon(total_actions, epsilon)
+        next_observation, reward, done = (None, 0., True) if done else env.step(action)
 
         experience = Experience(observation, action, reward, next_observation)
         buffer.add(experience)
@@ -239,10 +210,10 @@ def train(env_name='CartPole-v0',  batch_size=32, discount=0.99, lr=1e-3, render
 
         if total_actions % 50 == 0:
             experiences = buffer.sample(batch_size)
-            targets_batch = reward_batch + (1 - terminal_batch) * discount * sess.run(target_action_values, feed_dict={state_ph: next_state_batch})
+            state_batch, actions_batch, target_batch = get_targets(experiences)
             _loss, _ , _av = sess.run([loss, train_op, action_values], feed_dict={state_ph: state_batch,
-                                      targets_ph: targets_batch,
-                                      actions_taken: action_batch})
+                                      targets_ph: np.array(target_batch),
+                                      actions_taken: np.array(actions_batch)})
 
         if total_actions % target_update_freq == 0:
             copy_network_parameters(sess)
@@ -251,7 +222,6 @@ def train(env_name='CartPole-v0',  batch_size=32, discount=0.99, lr=1e-3, render
         if total_actions % 1000 == 0.0:
             ax.cla()
             ax.plot(returns)
-            plt.draw()
             plt.pause(0.1)
 
 
